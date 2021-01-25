4dd8d9eed1ad8942e3aa81a9bbe01231959a0780
Yosef-Ali <yosethio@yahoo.com>
2021-01-25 19:16:41 +0300

    backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7528f40a43e57a6cdc55754282f2788f'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4fdf2e48-68b1-4020-99b2-1ae83686bdb5'
+};
